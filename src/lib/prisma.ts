eb94e1ad2991d694761594abfe06d3eed6f12905
Atr-eus <an1ndya@proton.me>
2025-10-29 12:24:07 +0600

    use tmp file to add aiven cert dynamically

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const certPath = path.join("/tmp", "aiven-ca.pem");
+if (process.env.AIVEN_CA_CERT && !fs.existsSync(certPath)) {
+  fs.writeFileSync(certPath, process.env.AIVEN_CA_CERT);
+}
+
+const connectionString = `${process.env.DATABASE_URL}?sslmode=verify-full&sslrootcert=${certPath}`;
+
+const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter });
