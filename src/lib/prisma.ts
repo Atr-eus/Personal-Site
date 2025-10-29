ab13dc9b32d4e0c56d310964ba440f8728947824
Atr-eus <an1ndya@proton.me>
2025-10-29 21:20:03 +0600

    move to neondb from aiven

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,5 @@
-import fs from "fs";
-import path from "path";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const certPath = path.join("/tmp", "aiven-ca.pem");
-if (process.env.AIVEN_CA_CERT && !fs.existsSync(certPath)) {
-  fs.writeFileSync(certPath, process.env.AIVEN_CA_CERT);
-}
-
-const connectionString = `${process.env.DATABASE_URL}?sslmode=verify-full&sslrootcert=${certPath}`;
-
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
